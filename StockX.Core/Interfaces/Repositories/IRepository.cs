e7b926102acdd48b74a70236c492ff7adc7ba827
Vignesh J <vigneshjayakumar9221@gmail.com>
2026-02-25 10:33:26 +0530

    Implement IRepository

---
+++ StockX.Core/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,25 @@
+using System.Linq.Expressions;
+
+namespace StockX.Core.Interfaces;
+
+public interface IRepository<TEntity> where TEntity : class
+{
+    Task<TEntity?> GetByIdAsync(object id, CancellationToken cancellationToken = default);
+
+    Task<IReadOnlyList<TEntity>> GetAllAsync(CancellationToken cancellationToken = default);
+
+    Task<IReadOnlyList<TEntity>> FindAsync(
+        Expression<Func<TEntity, bool>> predicate,
+        CancellationToken cancellationToken = default);
+
+    Task AddAsync(TEntity entity, CancellationToken cancellationToken = default);
+
+    Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
+
+    void Update(TEntity entity);
+
+    void Remove(TEntity entity);
+
+    void RemoveRange(IEnumerable<TEntity> entities);
+}
+
